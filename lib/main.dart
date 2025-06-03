a0f711a23374e3626e671cba31cb076b702671ed
yfernandeza <yfernandeza@DESKTOP-BGGMNOI>
2025-06-03 19:53:48 -0400

    Definicion de theme para el formulario

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,15 @@ class MyApp extends StatelessWidget {
     //throw UnimplementedError();
     return MaterialApp(
       title: "Formulario de Registro",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF1E3A8A),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Color(0xFF1E3A8A),
+            secondary: Color(0xFF6B7280),
+        ),
+        useMaterial3: true,
+      ),
     );
   }
 }
